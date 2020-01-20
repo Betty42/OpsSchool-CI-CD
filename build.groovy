d758361138b58abc0ccb1222e47cba711042325a
Betty42 <55029687+Betty42@users.noreply.github.com>
2020-01-20 18:40:54 +0200

    create build.groovy

---
+++ build.groovy
@@ -0,0 +1,23 @@
+#! groovy
+
+node('docker') {
+
+  def customImage = ''
+  
+  stage('create dockerfile') {
+    sh '''
+    tee Dockerfile <<-'EOF'
+    FROM ubuntu:latest
+    RUN touch file-01.txt
+    EOF
+    '''
+  }
+
+  stage('build') {
+    customImage = docker.build("training/webapp")
+  }
+  
+  stage('test') {
+    sh 'docker images'
+  }
+}
